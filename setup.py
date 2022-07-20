eadc1a552651eb108abe151eeccbf690ee738613
LandonTheCoder <100165458+LandonTheCoder@users.noreply.github.com>
2022-07-20 18:56:36 -0400

    Start a setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+# Let's hope this works...
+from setuptools import setup
+##from distutils.core import setup
+
+#email: 100165458+LandonTheCoder@users.noreply.github.com
+setup(name="Scribe",
+      version="1.3.1",
+      description="TBD",
+      author="LandonTheCoder and aaravm10",
+      author_email="100165458+LandonTheCoder@users.noreply.github.com",
+      url="https://github.com/LandonTheCoder/bridgehacks-team-2-2022",
+      packages=[""],
+##      scripts=[""],
+      requires=["automatic_speech_recognition"]
+                   }
+     )
