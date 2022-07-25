69006b4a9297711cef6303714e02ed5e7fbd4c02
LandonTheCoder <100165458+LandonTheCoder@users.noreply.github.com>
2022-07-25 19:31:16 -0400

    Add Noteform demo This filters the text. I've edited it to functionize the code.

---
+++ scribe/noteform-demo.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3
+import noteform
+noteform.main(noteform.remlist)
--- scribe/noteform.py
+++ scribe/noteform.py
@@ -1,38 +1,25 @@
-mystr = " Turtles are very cool. They come in all shapes and sizes. They have green shells that protect them from danger"
+#! /usr/bin/python3
+mystr = ("Turtles are very cool.", "They come in all shapes and sizes.", "They have green shells that protect them from danger")
 
-remlist = ["that ", "the ", "what ", "and ", "them ", "they ","have ", "an ", "to",  "I ", "my ", "has ", "They" ]
-
-splist = mystr.split(".")
+remlist = ['that ', 'the ', 'what ', 'and ', 'them ', 'they ', 'have ', 'an ', 'to', 'I ', 'my ', 'has ', 'They', "." ]
 
 
+'''
+for i in range(len(splist)):
+    for word in remlist:
+        splist[i] = splist[i].replace(word, "")
 
 for i in range(len(splist)):
-  for word in remlist:
-    splist[i] = splist[i].replace(word, "")
-
-
-
-for i in range(len(splist)):
-  splist[i] = "-" + splist[i]
-
-
+    splist[i] = "-" + splist[i]
 
 for i in splist:
-  print(i)
+    print(i)
+'''
 
-
-
-
-
-
-
-
-    
-
-
-
-
-  
-
-
-
+def filter_sentence(sentence, rmlist=remlist):
+    for word in rmlist:
+        sentence = sentence.replace(word, "")
+    return " - " + sentence.strip()
+def main(rmlist):
+    for x in mystr:
+        print(filter_sentence(x))
