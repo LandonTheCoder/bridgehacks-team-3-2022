17772d672ea6795360e28b6e4a552dc0e18723ae
LandonTheCoder <100165458+LandonTheCoder@users.noreply.github.com>
2022-07-25 16:46:21 -0400

    Functionize scribe/recognize.py I've split up the code of scribe/recognize.py into functions, so that it is easily callable. Note the function main(), which implements the demo, and can take arbitrary file names as an argument. If a filename isn't passed, it assumes it to be "./speech-recognition-test.wav" (meaning a file named "speech-recognition-test.wav" in the current directory.
    Note that to execute the demo as a script, you now have to execute scribe/recognize-demo.py instead of scribe/recognize.py . Consequently, scribe/recognize.py doesn't have the executable permission set anymore on filesystems that store that permission.

---
+++ scribe/recognize-demo.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+import recognize
+
+recognize.main()
--- scribe/recognize.py
+++ scribe/recognize.py
@@ -3,21 +3,35 @@
 # Example loosely based on https://github.com/Uberi/speech_recognition/blob/master/examples/audio_transcribe.py
 
 import speech_recognition as sr
-rec = sr.Recognizer() # Performs action of recognizing speech
-FNAME = "./speech-recognition-test.wav"
-phraselist = []
-with sr.AudioFile(FNAME) as source:
-    while True: # Will break when nothing is returned from speech
-        audio = rec.listen(source) # Records 1 phrase
-        try:
-            text = rec.recognize_google(audio) # Google Speech Recognition has the best quality.
-            if text != "":
-                phraselist.append(text)
-            print("Google Speech Recognition thinks you said \"%s\"" % text)
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-            break;
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-            break;
-print("phraselist:", phraselist)
+
+def recognize_file(fobj):
+    """Return a list of phrases for a file object or filename. Accepts WAV or FLAC."""
+    rec = sr.Recognizer() # Performs action of recognizing speech
+    phraselist = []
+    with sr.AudioFile(fobj) as source:
+        while True: # Will break when nothing is returned from speech
+            audio = rec.listen(source) # Records 1 phrase
+            try:
+                text = rec.recognize_google(audio) # Google Speech Recognition has the best quality.
+                if text != "":
+                    phraselist.append(text)
+                print("Google Speech Recognition thinks you said \"%s\"" % text)
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+                break;
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+                break;
+    return phraselist
+
+def main(passed_args=None):
+    import argparse
+    parser = argparse.ArgumentParser(description="Load an audio file and recognize its contents. ")
+    parser.add_argument("file", help="The audio file to recognize. If unspecified, assumed to be \"./speech-recognition-test.wav\"", nargs='?')
+    args = parser.parse_args(passed_args)
+    FNAME = "./speech-recognition-test.wav"
+    if args.file == None:
+        phraselist = recognize_file(FNAME)
+    else:
+        phraselist = recognize_file(args.file)
+    print("phraselist:", phraselist)
